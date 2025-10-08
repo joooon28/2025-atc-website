f2f642fad5c373c7fbe17a6d565b8aa0691ae277
joonsu <junsu6580@gmail.com>
2025-10-08 12:36:03 +0900

    feat: update Program page

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -4,9 +4,10 @@ import Footer from "../components/Footer";
 export default function Program() {
   return (
     <div className="flex flex-col min-h-svh bg-mint-2">
-      <div className="py-[40px]">
+      <div className="sticky top-0 pt-[40px]">
         <Header />
       </div>
+      <div className="bg-mint-3 h-[1000px]"></div>
       <div className="mt-auto">
         <Footer />
       </div>
