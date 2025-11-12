beacb462ff28ead18c5dd914b20f823b07469c0b
joonsu <junsu6580@gmail.com>
2025-11-13 01:17:21 +0900

    fix: main plus, minus, volume button 색상 변경

--- src/components/main/PlusButton.jsx
+++ src/components/main/PlusButton.jsx
@@ -2,7 +2,7 @@ import { PlusIcon } from "@phosphor-icons/react";
 
 export default function PlusButton() {
   return (
-    <button className="hover:bg-fill hover:text-label-invert flex justify-center items-center border border-label w-[45px] h-[45px] bg-mint-4 p-3">
+    <button className="hover:bg-fill hover:text-label-invert flex justify-center items-center border border-label w-[45px] h-[45px] bg-mint-5 p-3">
       <PlusIcon className="w-[21px] h-[21px]" weight="thin" />
     </button>
   );
