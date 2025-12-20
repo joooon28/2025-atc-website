77a29824608f9d083cb0c7b090a30071cee3c8ec
jellykeeng <loginnetwork4@gmail.com>
2025-12-20 18:59:00 +0900

    Work 메인 이미지 추가

--- src/data/work/WorkDetailArtistInfo.js
+++ src/data/work/WorkDetailArtistInfo.js
@@ -6,7 +6,7 @@ export const allArtworkData = {
     artistKr: "손정우",
     artistEn: "Jeongwoo Son",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954496/-_%EC%86%90%EC%A0%95%EC%9A%B0_qpc0nk.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921607/-_%EC%86%90%EC%A0%95%EC%9A%B0_qpc0nk_jxggaz.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952755/_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_unxile.png",
     videoSrc: "https://vimeo.com/1136010116?fl=pl&fe=sh",
@@ -38,7 +38,7 @@ export const allArtworkData = {
     artistKr: "도티즈",
     artistEn: "dotties",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762960860/%EB%8B%B7_%EC%8D%B8%EB%84%A4%EC%9D%BC_i5dtog.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921663/%EB%8B%B7_%EC%8D%B8%EB%84%A4%EC%9D%BC_i5dtog_tmtt4u.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762960881/%EB%8B%B7_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_gcjg0l.jpg",
     videoSrc: "",
@@ -99,7 +99,7 @@ export const allArtworkData = {
     artistKr: "키스 갈기기",
     artistEn: "Throwing Kith",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954511/%EB%A9%B4%ED%92%8D2_%EC%8D%B8%EB%84%A4%EC%9D%BC_mnkbnb.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921652/%EB%A9%B4%ED%92%8D2_%EC%8D%B8%EB%84%A4%EC%9D%BC_mnkbnb_eyljyv.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952768/%EB%A9%B4%ED%92%8D2_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_ww3yvm.png",
     videoSrc: "",
@@ -145,7 +145,7 @@ export const allArtworkData = {
     artistKr: "자발적 과로단",
     artistEn: "self over workers",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954513/%EB%AC%BC%EC%88%A8_%EC%8D%B8%EB%84%A4%EC%9D%BC_l581j4.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921655/%EB%AC%BC%EC%88%A8_%EC%8D%B8%EB%84%A4%EC%9D%BC_l581j4_fdshb3.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952767/%EB%AC%BC%EC%88%A8_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_wu4rmd.png",
     videoSrc: "",
@@ -211,7 +211,7 @@ export const allArtworkData = {
     artistKr: "로피",
     artistEn: "Roffi",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954514/%EB%B8%94%EB%A3%A8%EB%84%A4%EA%B1%B0%ED%8B%B0%EB%B8%8C_%EC%8D%B8%EB%84%A4%EC%9D%BC_cdzwzz.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921670/%EB%B8%94%EB%A3%A8%EB%84%A4%EA%B1%B0%ED%8B%B0%EB%B8%8C_%EC%8D%B8%EB%84%A4%EC%9D%BC_cdzwzz_jta2z6.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952772/%EB%B8%94%EB%A3%A8%EB%84%A4%EA%B1%B0%ED%8B%B0%EB%B8%8C_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_ohyqff.png",
     videoSrc: "",
@@ -285,7 +285,7 @@ export const allArtworkData = {
     artistKr: "맷돌 스튜디오",
     artistEn: "Maetdol Studio",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954516/%EC%82%B6%EC%9D%80%EC%84%A4%ED%99%94_%EB%A7%B7%EB%8F%8C%EC%8A%A4%ED%8A%9C%EB%94%94%EC%98%A4__%EC%8D%B8%EB%84%A4%EC%9D%BC_%EC%9D%B4%EB%AF%B8%EC%A7%80_-_%EC%9D%B4%EB%8B%A4%EC%9D%80_mej1dm.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921666/%EC%82%B6%EC%9D%80%EC%84%A4%ED%99%94_%EB%A7%B7%EB%8F%8C%EC%8A%A4%ED%8A%9C%EB%94%94%EC%98%A4__%EC%8D%B8%EB%84%A4%EC%9D%BC_%EC%9D%B4%EB%AF%B8%EC%A7%80_-_%EC%9D%B4%EB%8B%A4%EC%9D%80_mej1dm_zubklw.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763036726/%EC%82%B6%EC%9D%80%EC%84%A4%ED%99%94_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_ug0knc.png",
     videoSrc: "",
@@ -348,7 +348,7 @@ export const allArtworkData = {
     artistKr: "뚜앙즈",
     artistEn: "Tuangz",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954517/%EC%8B%AC_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EC%8B%9C%EB%A6%AC%EB%A1%B1_vyx7ua.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921600/%EC%8B%AC_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EC%8B%9C%EB%A6%AC%EB%A1%B1_vyx7ua_u4dfem.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952773/%EC%8B%AC_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_njdesc.png",
     videoSrc: "https://vimeo.com/1135770875?fl=pl&fe=sh",
@@ -403,7 +403,7 @@ export const allArtworkData = {
     artistKr: "야너이상해",
     artistEn: "Hey, What's wrong with you?",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954519/%EC%95%84%EC%A7%80%ED%8A%B8_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EC%8B%A0%EC%84%9C%EC%9C%A4_imnumt.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921614/%EC%95%84%EC%A7%80%ED%8A%B8_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EC%8B%A0%EC%84%9C%EC%9C%A4_imnumt_v3m1hj.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952775/%EC%95%84%EC%A7%80%ED%8A%B8_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_ovikzr.png",
     videoSrc: "",
@@ -453,7 +453,7 @@ export const allArtworkData = {
     artistKr: "와이칼리버",
     artistEn: "Ycalibur",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954527/%EC%97%90%EB%A6%B0%EC%9D%98%EC%8B%A0%EB%93%A4_%EC%8D%B8%EB%84%A4%EC%9D%BC_vwuvnw.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921605/%EC%97%90%EB%A6%B0%EC%9D%98%EC%8B%A0%EB%93%A4_%EC%8D%B8%EB%84%A4%EC%9D%BC_vwuvnw_v1rcsy.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952776/%EC%97%90%EB%A6%B0%EC%9D%98%EC%8B%A0%EB%93%A4_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_vq8ely.png",
     videoSrc: "",
@@ -494,7 +494,7 @@ export const allArtworkData = {
     artistKr: "아마따",
     artistEn: "Amatuer",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954523/%EC%98%81%EC%B4%88_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EC%8B%A0%EC%84%9C%EC%9C%A4_jv1o1y.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921645/%EC%98%81%EC%B4%88_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EC%8B%A0%EC%84%9C%EC%9C%A4_jv1o1y_ahlph9.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952753/%EC%98%81%EC%9B%90%EC%9D%98%EC%B4%88%EC%83%81_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_twufpe.jpg",
     videoSrc: "",
@@ -536,7 +536,7 @@ export const allArtworkData = {
     artistKr: "투진스",
     artistEn: "2genes",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954524/%EC%98%81%EC%9B%90%ED%95%9C%EC%97%AC%EB%A6%84_%EC%8D%B8%EB%84%A4%EC%9D%BC_kj28ee.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921710/%EC%98%81%EC%9B%90%ED%95%9C%EC%97%AC%EB%A6%84_%EC%8D%B8%EB%84%A4%EC%9D%BC_kj28ee_ljy0fz.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763036732/%EC%9B%B9%EC%82%AC%EC%9D%B4%ED%8A%B8_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_jjjxkm.png",
     videoSrc: "",
@@ -581,7 +581,7 @@ export const allArtworkData = {
     artistKr: "더 스님스",
     artistEn: "The Seunims",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954524/%EC%98%B4_%EC%8D%B8%EB%84%A4%EC%9D%BC_khk6hr.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921647/%EC%98%B4_%EC%8D%B8%EB%84%A4%EC%9D%BC_khk6hr_hhhh1b.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952755/%EC%98%B4_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_eh1lzd.jpg",
     videoSrc: "",
@@ -630,7 +630,7 @@ export const allArtworkData = {
     artistKr: "기록도록",
     artistEn: "GRDR",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954526/%EC%9A%B0%EC%9E%8A%EB%A7%90_%EC%8D%B8%EB%84%A4%EC%9D%BC_ne9trv.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921690/%EC%9A%B0%EC%9E%8A%EB%A7%90_%EC%8D%B8%EB%84%A4%EC%9D%BC_ne9trv_hlz25n.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952779/%EC%9A%B0%EC%9E%8A%EB%A7%90_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_pjlfr2.png",
     videoSrc: "",
@@ -692,7 +692,7 @@ export const allArtworkData = {
     artistKr: "비상명(非常名)",
     artistEn: "Project Kṣaṇa",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954528/%EC%B0%B0%EB%82%98%EB%B9%84_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EB%A5%98%ED%95%B4%EC%84%B1_h4pm5i.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921653/%EC%B0%B0%EB%82%98%EB%B9%84_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EB%A5%98%ED%95%B4%EC%84%B1_h4pm5i_iuttnn.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763036738/%EC%B0%B0%EB%B0%94%EB%8B%88_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_bs46pa.jpg",
     videoSrc: "",
@@ -746,7 +746,7 @@ export const allArtworkData = {
     artistKr: "노릇노릇",
     artistEn: "NRNR",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762953843/ToastOut_%EC%8D%B8%EB%84%A4%EC%9D%BC%EC%9D%B4%EB%AF%B8%EC%A7%80_enjz5l.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921604/ToastOut_%EC%8D%B8%EB%84%A4%EC%9D%BC%EC%9D%B4%EB%AF%B8%EC%A7%80_enjz5l_rxqfvl.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952763/ToastOut_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_wxp1qs.png",
     videoSrc: "",
@@ -797,7 +797,7 @@ export const allArtworkData = {
     artistKr: "서탭입니다",
     artistEn: "We're steff",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954509/%EA%B0%90%EA%B0%81%EC%9D%98%EC%88%B2_%EC%8D%B8%EB%84%A4%EC%9D%BC_i0inan.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921651/%EA%B0%90%EA%B0%81%EC%9D%98%EC%88%B2_%EC%8D%B8%EB%84%A4%EC%9D%BC_i0inan_x5cghm.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763077320/%EA%B0%90%EA%B0%81%EC%9D%98%EC%88%B2_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_ph7qha.png",
     videoSrc: "",
@@ -862,7 +862,7 @@ export const allArtworkData = {
     artistKr: "파브리카",
     artistEn: "Fabrika",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954532/%ED%8C%8C%EB%B8%8C%EB%A6%AC%EC%B9%B4_%EC%8D%B8%EB%84%A4%EC%9D%BC_jh3yto.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921664/%ED%8C%8C%EB%B8%8C%EB%A6%AC%EC%B9%B4_%EC%8D%B8%EB%84%A4%EC%9D%BC_jh3yto_ewkk80.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952754/%ED%8C%8C%EB%B8%8C%EB%A6%AC%EC%B9%B4_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_uszmve.jpg",
     videoSrc: "",
@@ -904,7 +904,7 @@ export const allArtworkData = {
     artistKr: "",
     artistEn: "X205",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954532/%ED%97%88%EB%A6%AC%ED%94%BC%EC%9E%90_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_Jiyun_Kim_gwowms.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921601/%ED%97%88%EB%A6%AC%ED%94%BC%EC%9E%90_%EC%8D%B8%EB%84%A4%EC%9D%BC_-_Jiyun_Kim_gwowms_ohvyhu.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952780/%ED%97%88%EB%A6%AC%ED%94%BC%EC%9E%90_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_slcnbe.png",
     videoSrc: "",
@@ -972,7 +972,7 @@ export const allArtworkData = {
     artistKr: "라스트 댄스",
     artistEn: "Last Dance",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762965841/OMI_%EC%8D%B8%EB%84%A4%EC%9D%BC_h2qxyy.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921611/OMI_%EC%8D%B8%EB%84%A4%EC%9D%BC_h2qxyy_hiaaif.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763077428/OMI_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_yv9a1v.png",
     videoSrc: "",
@@ -1013,7 +1013,7 @@ export const allArtworkData = {
     artistKr: "",
     artistEn: "ACTA",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954501/D31_Thumbnail_Image_-_%EC%98%A4%EC%A3%BC%ED%95%98_fq20vt.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921686/D31_Thumbnail_Image_-_%EC%98%A4%EC%A3%BC%ED%95%98_fq20vt_yp4whw.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763036423/D31_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_dngzxj.png",
     videoSrc: "",
@@ -1079,7 +1079,7 @@ export const allArtworkData = {
     artistKr: "",
     artistEn: "CONTINUE?",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954500/E.C.H.O._%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EC%B0%A8%EC%84%B8%EC%97%B0_ygczd4.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921616/E.C.H.O._%EC%8D%B8%EB%84%A4%EC%9D%BC_-_%EC%B0%A8%EC%84%B8%EC%97%B0_ygczd4_gsndii.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952761/E.C.H.O._%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_zlubc6.png",
     videoSrc: "",
@@ -1127,7 +1127,7 @@ export const allArtworkData = {
     artistKr: "이건 네 번째 레슨",
     artistEn: "The fourth lesson",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954507/web-sight_%EC%8D%B8%EB%84%A4%EC%9D%BC_e9hxpo.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921602/web-sight_%EC%8D%B8%EB%84%A4%EC%9D%BC_e9hxpo_tp6k9s.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952770/web-stie_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_fhhsyh.png",
     videoSrc: "",
@@ -1181,7 +1181,7 @@ export const allArtworkData = {
     artistKr: "김민수",
     artistEn: "Min Soo Kim",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762955526/MusicalDream_%EC%8D%B8%EB%84%A4%EC%9D%BC_jir0wp.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921639/MusicalDream_%EC%8D%B8%EB%84%A4%EC%9D%BC_jir0wp_xomw7l.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762955539/MusicalDream_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_vy4irl.png",
     videoSrc: "",
@@ -1213,7 +1213,7 @@ export const allArtworkData = {
     artistKr: "이윤선",
     artistEn: "Yoonseon Lee",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954501/Faces_%EC%8D%B8%EB%84%A4%EC%9D%BC_xqws7o.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921668/Faces_%EC%8D%B8%EB%84%A4%EC%9D%BC_xqws7o_gwrxhh.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952761/Faces_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_z2gzjx.png",
     videoSrc: "",
@@ -1245,7 +1245,7 @@ export const allArtworkData = {
     artistKr: "디지털 기억 재단",
     artistEn: "Digital Memory Foundation",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954509/RIP_%EC%8D%B8%EB%84%A4%EC%9D%BC_a5nosj.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921705/RIP_%EC%8D%B8%EB%84%A4%EC%9D%BC_a5nosj_nz8iry.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763036677/RIP_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_nz55ph.png",
     videoSrc: "https://vimeo.com/1092433449?fl=pl&fe=sh",
@@ -1300,7 +1300,7 @@ export const allArtworkData = {
     artistKr: "두바구",
     artistEn: "DBG",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762957086/SnowBall_%EC%8D%B8%EB%84%A4%EC%9D%BC_zgeg1s.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921660/SnowBall_%EC%8D%B8%EB%84%A4%EC%9D%BC_zgeg1s_g0tiij.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762957056/Snowball_Effect_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_uiahra.jpg",
     videoSrc: "https://vimeo.com/1135737755?fl=pl&fe=sh",
@@ -1361,7 +1361,7 @@ export const allArtworkData = {
     artistKr: "",
     artistEn: "Neuverse Lab",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954504/Neuverse_Thumbnail_-_SunMyeong_Lee_budira.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921612/Neuverse_Thumbnail_-_SunMyeong_Lee_budira_ox3dgg.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763036757/Neuverse_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_mpzs1l.png",
     videoSrc: "https://vimeo.com/1138429497?fl=pl&fe=sh",
@@ -1429,7 +1429,7 @@ export const allArtworkData = {
     artistKr: "티씨케이",
     artistEn: "TCK",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954529/%ED%8B%B0%ED%8B%B0%ED%8B%B0_%EC%8D%B8%EB%84%A4%EC%9D%BC_%EC%B5%9C%EC%A2%85%EB%B3%B8_-_Seoyoung_Kim_ddurni.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921658/%ED%8B%B0%ED%8B%B0%ED%8B%B0_%EC%8D%B8%EB%84%A4%EC%9D%BC_%EC%B5%9C%EC%A2%85%EB%B3%B8_-_Seoyoung_Kim_ddurni_sgpdz2.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952765/TTT_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_ewagbq.png",
     videoSrc: "",
@@ -1480,7 +1480,7 @@ export const allArtworkData = {
     artistKr: "신채원",
     artistEn: "Chaewon Shin",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954499/e-mo_%EC%8D%B8%EB%84%A4%EC%9D%BC_plxtn7.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921659/e-mo_%EC%8D%B8%EB%84%A4%EC%9D%BC_plxtn7_l7qonr.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952759/e-mo_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_vx1d1o.png",
     videoSrc: "",
@@ -1515,7 +1515,7 @@ export const allArtworkData = {
     artistKr: "커비",
     artistEn: "KIRBY",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763539018/GlassLab_%EC%8D%B8%EB%84%A4%EC%9D%BC_zkrmuv.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921706/GlassLab_%EC%8D%B8%EB%84%A4%EC%9D%BC_zkrmuv_b6j0wl.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1763036748/GLASSLAB_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_s5vbdt.png",
     videoSrc: "",
@@ -1569,7 +1569,7 @@ export const allArtworkData = {
     artistKr: "노진서",
     artistEn: "Noh jinseo",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954497/Chroma_%EC%8D%B8%EB%84%A4%EC%9D%BC_v0p9yb.png",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921643/Chroma_%EC%8D%B8%EB%84%A4%EC%9D%BC_v0p9yb_nfejoj.png",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952758/chroma_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_qu7kiy.png",
     videoSrc: "",
@@ -1639,7 +1639,7 @@ export const allArtworkData = {
     artistKr: "토마토마토",
     artistEn: "tomat()mato",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762960856/ART_IST_%EC%8D%B8%EB%84%A4%EC%9D%BC_qgbrmz.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921650/ART_IST_%EC%8D%B8%EB%84%A4%EC%9D%BC_qgbrmz_zyvzuv.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762960880/ART_IST_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_mhcspn.jpg",
     videoSrc: "",
@@ -1693,7 +1693,7 @@ export const allArtworkData = {
     artistKr: "김태현",
     artistEn: "Taehyun Kim",
     imageMainSrc:
-      "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762954509/WithLily_%EC%8D%B8%EB%84%A4%EC%9D%BC_kie6bf.jpg",
+      "https://res.cloudinary.com/dzwooybws/image/upload/v1764921610/WithLily_%EC%8D%B8%EB%84%A4%EC%9D%BC_kie6bf_clvnyt.jpg",
     imageDetailSrc:
       "https://res.cloudinary.com/dbw1ckgzr/image/upload/v1762952753/WithLily_%EB%82%B4%EB%B6%80%EC%9D%B4%EB%AF%B8%EC%A7%80_s8dpvr.jpg",
     videoSrc: "",
